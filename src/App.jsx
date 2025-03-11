9c37122faaaec6f3bc2c09b62c6251026491e60c
UmbraFlare-code <umaxuel@gmail.com>
2025-03-11 10:25:13 -0500

    resolver letra de error en header de app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header/header";
+import Header from "./components/Header/Header";
 import "./components/Header/Header.css";
 import About from "./components/About/About";
 import "./components/About/About.css";
